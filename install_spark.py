e7d8aafd154891581610ca43a8d7097bd140624b
Melih <melih3553@gmail.com>
2024-01-23 10:09:21 +0000

    spark installation py file updated

--- install_spark.py
+++ install_spark.py
@@ -3,14 +3,29 @@
 import subprocess
 
 def run_command(command):
-    subprocess.run(command, shell=True, check=True)
+    print(f"Running command: {command}")
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    while True:
+        output = process.stdout.readline()
+        error = process.stderr.readline()
+        if output == b'' and error == b'' and process.poll() is not None:
+            break
+        if output:
+            print(output.decode('utf-8').strip())
+        if error:
+            print(error.decode('utf-8').strip())
+
+    return process.returncode
 
 def install_spark():
     # Update package lists
     run_command("sudo apt-get update -qq")
 
     # Install OpenJDK
-    run_command("sudo apt-get install openjdk-8-jdk-headless -qq > /dev/null")
+    result = run_command("sudo apt install -y openjdk-8-jdk-headless")
+    if result != 0:
+        print("Error installing OpenJDK. Exiting.")
+        return
 
     # Download and extract Spark
     run_command("sudo wget -q http://archive.apache.org/dist/spark/spark-3.1.1/spark-3.1.1-bin-hadoop3.2.tgz")
